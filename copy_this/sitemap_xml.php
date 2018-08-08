f194b2a6a26fc5030264187621d96cca264b85a0
Ivo Bathke <ivo.bathke@gmail.com>
2018-08-08 11:25:05 +0200

    sitemap_generator.php

--- copy_this/sitemap_xml.php
+++ copy_this/sitemap_xml.php
@@ -5,22 +5,25 @@
 
 require_once __DIR__.'/bootstrap.php';
 
-$aLangParams = \oxRegistry::getConfig()->getConfigParam('aLanguageParams');
-$shopUrl     = oxRegistry::get("oxConfigFile")->getVar("sShopURL");
-$config      = new \Ivoba\OxidSiteMap\Entity\Config(__DIR__, 'sitemap.xml', $shopUrl, $aLangParams);
+use \OxidEsales\Eshop\Core\Registry;
+use \OxidEsales\Eshop\Core\DatabaseProvider;
 
-$db = oxDb::getDb(\oxDb::FETCH_MODE_ASSOC);
+$aLangParams = Registry::getConfig()->getConfigParam('aLanguageParams');
+$shopUrl     = Registry::get("oxConfigFile")->getVar('sShopURL');
+$config      = new \IvobaOxid\OxidSiteMap\Entity\Config(__DIR__, 'sitemap.xml', $shopUrl, $aLangParams);
+
+$db = DatabaseProvider::getDb(DatabaseProvider::FETCH_MODE_ASSOC);
 
 $queries = [
-    new \Ivoba\OxidSiteMap\Query\Categories($db, $config, $hierachy = '0.8', $changefreq = 'weekly'),
-    new \Ivoba\OxidSiteMap\Query\HiddenCategories($db, $config, $hierachy = '0.8', $changefreq = 'weekly'),
-    new \Ivoba\OxidSiteMap\Query\Cms($db, $config, $hierachy = '0.1', $changefreq = 'weekly'),
-    new \Ivoba\OxidSiteMap\Query\Tags($db, $config, $hierachy = '0.1', $changefreq = 'weekly'),
-    new \Ivoba\OxidSiteMap\Query\Products($db, $config, $hierachy = '1.0', $changefreq = 'daily'),
+    new \IvobaOxid\OxidSiteMap\Query\Categories($db, $config, $hierachy = '0.8', $changefreq = 'weekly'),
+    new \IvobaOxid\OxidSiteMap\Query\HiddenCategories($db, $config, $hierachy = '0.8', $changefreq = 'weekly'),
+    new \IvobaOxid\OxidSiteMap\Query\Cms($db, $config, $hierachy = '0.1', $changefreq = 'weekly'),
+    new \IvobaOxid\OxidSiteMap\Query\Tags($db, $config, $hierachy = '0.1', $changefreq = 'weekly'),
+    new \IvobaOxid\OxidSiteMap\Query\Products($db, $config, $hierachy = '1.0', $changefreq = 'daily'),
 ];
 
-$urlFilter        = new \Ivoba\OxidSiteMap\Filter\UrlFilter([$shopUrl.'/AGB/']);
-$siteMapGenerator = new \Ivoba\OxidSiteMap\SiteMapGenerator($config, $queries, true, [$urlFilter]);
+$urlFilter        = new \IvobaOxid\OxidSiteMap\Filter\UrlFilter([$shopUrl.'/AGB/']);
+$siteMapGenerator = new \IvobaOxid\OxidSiteMap\SiteMapGenerator($config, $queries, true, [$urlFilter]);
 
 $siteMapGenerator->generate();
 
